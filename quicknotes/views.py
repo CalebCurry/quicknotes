9f29cd4b0033e401a78176ab8012121b57a8277e
Caleb Curry <caleb@calebcurry.com>
2025-09-02 04:36:54 -0400

    add viewset

--- quicknotes/views.py
+++ quicknotes/views.py
@@ -1,9 +1,16 @@
 from django.http import HttpResponse, JsonResponse
+
+from quicknotes.serializers import NoteSerializer
 from .models import Note
+from rest_framework.viewsets import ModelViewSet
 
 def home(request):
     return HttpResponse("Welcome Home!")
 
-def api_notes(request):
-    data = list(Note.objects.values())
-    return JsonResponse({'notes': data })
+# def api_notes(request):
+#     data = list(Note.objects.values())
+#     return JsonResponse({'notes': data })
+
+class NoteViewSet(ModelViewSet):
+    queryset = Note.objects.all()
+    serializer_class = NoteSerializer
