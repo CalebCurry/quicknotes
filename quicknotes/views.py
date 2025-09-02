576a97d8279d483f4ddc01e88d7d40e0ad7d09cd
Caleb Curry <caleb@calebcurry.com>
2025-09-02 06:06:24 -0400

    nest in data attribute

--- quicknotes/views.py
+++ quicknotes/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse, JsonResponse
 from quicknotes.serializers import NoteSerializer
 from .models import Note
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.response import Response 
 
 def home(request):
     return HttpResponse("Welcome Home!")
@@ -14,3 +15,13 @@ def home(request):
 class NoteViewSet(ModelViewSet):
     queryset = Note.objects.all()
     serializer_class = NoteSerializer
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+        serializer = self.get_serializer(queryset, many=True)
+        return Response({"data": serializer.data})
+    
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance)
+        return Response({"data": serializer.data})
