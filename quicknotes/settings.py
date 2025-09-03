d0647c8c16b4e27b004412ac21da8dd2e598c15d
Caleb Curry <caleb@calebcurry.com>
2025-09-03 14:53:04 -0400

    switch to local postgres container

--- quicknotes/settings.py
+++ quicknotes/settings.py
@@ -77,11 +77,16 @@ WSGI_APPLICATION = 'quicknotes.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "postgres",
+        "USER": "postgres",
+        "PASSWORD": "password",
+        "HOST": "127.0.0.1",
+        "PORT": "5431",
     }
 }
+
 
 
 # Password validation
