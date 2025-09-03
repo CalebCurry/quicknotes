5b6f50c06a719c91c131fd27c076b88aff1da025
Caleb Curry <caleb@calebcurry.com>
2025-09-03 15:52:55 -0400

    basic collection viewset

--- quicknotes/serializers.py
+++ quicknotes/serializers.py
@@ -1,10 +1,16 @@
 from rest_framework.serializers import ModelSerializer
 from rest_framework import serializers
-from quicknotes.models import Note
+from quicknotes.models import Collection, Note
 
 class NoteSerializer(ModelSerializer):
     #content = serializers.CharField(required=False, allow_blank=True, default="")
 
     class Meta:
         model = Note
+        fields = "__all__"
+
+class CollectionSerializer(ModelSerializer):
+
+    class Meta:
+        model = Collection
         fields = "__all__"
--- quicknotes/views.py
+++ quicknotes/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse, JsonResponse
 
-from quicknotes.serializers import NoteSerializer
-from .models import Note
+from quicknotes.serializers import CollectionSerializer, NoteSerializer
+from .models import Collection, Note
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.response import Response 
 
@@ -15,6 +15,20 @@ def home(request):
 class NoteViewSet(ModelViewSet):
     queryset = Note.objects.all()
     serializer_class = NoteSerializer
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+        serializer = self.get_serializer(queryset, many=True)
+        return Response({"data": serializer.data})
+    
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance)
+        return Response({"data": serializer.data})
+    
+class CollectionViewSet(ModelViewSet):
+    queryset = Collection.objects.all()
+    serializer_class = CollectionSerializer
 
     def list(self, request, *args, **kwargs):
         queryset = self.filter_queryset(self.get_queryset())
