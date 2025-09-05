1f5e91a0d05a57171447efc0051dea2d9699f152
Caleb Curry <caleb@calebcurry.com>
2025-09-05 17:39:19 -0400

    example /notes/ endpoint on a collection

--- quicknotes/views.py
+++ quicknotes/views.py
@@ -1,9 +1,10 @@
 from django.http import HttpResponse, JsonResponse
 
-from quicknotes.serializers import CollectionSerializer, NoteSerializer
+from quicknotes.serializers import CollectionSerializer, CollectionWithNotesSerializer, NoteSerializer
 from .models import Collection, Note
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.response import Response 
+from rest_framework.decorators import action
 #from django.db import connection
 
 def home(request):
@@ -32,7 +33,7 @@ class NoteViewSet(ModelViewSet):
         instance = self.get_object()
         serializer = self.get_serializer(instance)
         return Response({"data": serializer.data})
-    
+
 class CollectionViewSet(ModelViewSet):
     queryset = Collection.objects.all()
     serializer_class = CollectionSerializer
@@ -45,4 +46,16 @@ class CollectionViewSet(ModelViewSet):
     def retrieve(self, request, *args, **kwargs):
         instance = self.get_object()
         serializer = self.get_serializer(instance)
+        return Response({"data": serializer.data})
+    
+    @action(detail=True, methods=["GET"])
+    def notes(self, request, pk=None):
+        collection = Collection.objects.prefetch_related("notes").get(pk=pk)
+
+        # serializer = CollectionSerializer(collection)
+        # serializer_notes = NoteSerializer(collection.notes, many=True) # type: ignore
+        
+        # return Response({"data": {**dict(serializer.data), "notes": serializer_notes.data}})
+
+        serializer = CollectionWithNotesSerializer(collection)
         return Response({"data": serializer.data})
