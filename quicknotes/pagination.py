9ecb0e7f1b01905185e283485f97b0c17c0b46b3
Caleb Curry <caleb@calebcurry.com>
2025-09-06 13:28:15 -0400

    cursor pagination

--- quicknotes/pagination.py
+++ quicknotes/pagination.py
@@ -1,13 +1,14 @@
-from rest_framework.pagination import PageNumberPagination, LimitOffsetPagination
+from rest_framework.pagination import CursorPagination
 from rest_framework.response import Response
 
-class CustomPagination(LimitOffsetPagination):
-    default_limit = 20
-    max_limit = 100
+class CustomPagination(CursorPagination):
+    ordering = "id"
+    page_size = 20
+    page_size_query_param = "page_size"
+    max_page_size = 100
 
     def get_paginated_response(self, data):
         return Response({
-            "count": self.count,
             "next": self.get_next_link(),
             "previous": self.get_previous_link(),
             "data": data
