2738ea28074c249ee80400d34be08fc6bccd28f8
Caleb Curry <caleb@calebcurry.com>
2025-09-12 14:21:52 -0400

    progress

--- quicknotes-frontend/src/sdk/api.ts
+++ quicknotes-frontend/src/sdk/api.ts
@@ -1,9 +1,11 @@
 import axios from "axios";
 
+const jwt = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzU3NjQ5MDU0LCJpYXQiOjE3NTc2NDYwNTQsImp0aSI6ImY1Mjc1ZTlkODIyMDQzYTA5ZTQ4YjVjMjM4ZTA1NGFhIiwidXNlcl9pZCI6IjEifQ.g0us1y-qu61wlO5oSgeku-3yxm5wOP8T4kWeO0EmqU8';
+
 const api = axios.create({
   baseURL: "http://webdev:8000",
   headers: {
-    Authorization: "Bearer 123"
+    Authorization: `Bearer ${jwt}`
   }
 });
 
