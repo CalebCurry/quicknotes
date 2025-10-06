45a6cc1038e68d145470ad31073f528856e89e3e
Caleb Curry <caleb@calebcurry.com>
2025-10-06 15:59:30 -0400

    refreshing WIP

--- quicknotes-frontend/src/sdk/api.ts
+++ quicknotes-frontend/src/sdk/api.ts
@@ -1,13 +1,49 @@
 import axios from "axios";
 
-const jwt =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxMDM5NzcwNzc1NSwiaWF0IjoxNzU3Nzk0MTU1LCJqdGkiOiIzZDAzMjI5ZWU0NjI0MGQ5ODMwYjJmYWE1ZmQwZWY4YyIsInVzZXJfaWQiOiIxIn0.vigR__GU_YbuOBpSplu6TSPb0td4LFE_3wunR3BLqu4"
+//const jwt =
+//  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxMDM5NzcwNzc1NSwiaWF0IjoxNzU3Nzk0MTU1LCJqdGkiOiIzZDAzMjI5ZWU0NjI0MGQ5ODMwYjJmYWE1ZmQwZWY4YyIsInVzZXJfaWQiOiIxIn0.vigR__GU_YbuOBpSplu6TSPb0td4LFE_3wunR3BLqu4"
 const api = axios.create({
   baseURL: "http://localhost:8000",
   headers: {
     "Content-Type": "application/json",
-    Authorization: `Bearer ${jwt}`,
+//    Authorization: `Bearer ${jwt}`,
   },
+});
+
+api.interceptors.request.use((config) => {
+  console.log("request interceptor hit")
+  const access = localStorage.getItem("access");
+  if (access){
+    config.headers.Authorization = `Bearer ${access}`;
+  }
+  return config;
+}, (error) => Promise.reject(error));
+
+api.interceptors.response.use((response) => {
+  console.log("response interceptor hit")
+  return response;
+}, async (error) => {
+  if(error.response.status == 401 && !error.config.url.includes('/api/auth/refresh') && !error.config._retry){
+    error.config._retry = true;
+    //refresh
+    console.log("Refreshing access token");
+    try {
+      const refresh = localStorage.getItem("refresh");
+      const res = await api.post("/api/auth/refresh/", { refresh });
+      
+      const newAccess = res.data.access;
+      const newRefresh = res.data.refresh;
+
+      localStorage.setItem("access", newAccess);
+      localStorage.setItem("refresh", newRefresh);
+
+      console.log("NEW TOKENS", newAccess, newRefresh);
+      error.config.headers.Authorization = `Bearer ${newAccess}`;
+      return api.request(error.config);
+    } catch(refreshErr){
+      Promise.reject(refreshErr);
+    }
+  }
 });
 
 export type Collection = {
